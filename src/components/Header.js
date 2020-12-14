05dbcd84bcda2b383316528548b56ac9a7e306eb
Menachem Neiman <Nerevar123@gmail.com>
2020-12-14 03:19:11 +0200

    add HTML

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { HashLink as Link } from 'react-router-hash-link';
+import { NavHashLink as NavLink } from 'react-router-hash-link';
+
+function Header() {
+  return (
+    <>
+    </>
+  )
+}
+
+export default Header;
