6bfea6ceb292e96d21e4973f9a73bdcab194690a
Menachem Neiman <Nerevar123@gmail.com>
2021-01-18 12:44:07 +0200

    small fixes

--- frontend/frontend/src/utils/utils.js
+++
@@ -1,9 +0,0 @@
-const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://api.stone.ner.works/';
-// const baseUrl = 'https://api.stone.ner.works';
-
-export const apiOptions = {
-  baseUrl: baseUrl,
-  headers: {
-    "Content-Type": "application/json",
-  },
-};
