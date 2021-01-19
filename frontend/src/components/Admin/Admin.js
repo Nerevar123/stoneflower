596b6ed81e38d3b494788bb6273a4c8da8b17b55
Alexander Sarychev <alexander@sarychev.su>
2021-01-19 22:25:20 +0300

    test routing

--- frontend/src/components/Admin/Admin.js
+++ frontend/src/components/Admin/Admin.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
-import AdminMenu from "./AdminMenu";
+import React, { useState, useEffect, useRef, createRef } from "react";
 import {
   Router,
   Switch,
@@ -8,17 +7,77 @@ import {
   useRouteMatch,
   useHistory,
 } from "react-router-dom";
+import { pricing } from "../../utils/config";
 
 function Admin(props) {
-  const history = useHistory();
+  const [selectedItem, setSelectedItem] = useState("requests");
+  const adminItems = [
+    { id: "requests", content: "Заявки" },
+    { id: "lead", content: "Главная" },
+    { id: "services", content: "Услуги" },
+    { id: "advantages", content: "Преимущества" },
+    { id: "disadvantages", content: "Недостатки" },
+    { id: "infostages", content: "Этапы работы" },
+    { id: "pricing", content: "Расчёт цен" },
+    { id: "surfaces", content: "Варианты поверхностей" },
+    { id: "advices", content: "Советы дизайнера" },
+    { id: "portfolio", content: "Портфолио" },
+    { id: "suppliers", content: "Производители" },
+    { id: "postform", content: "Оставить заявку" },
+    { id: "contacts", content: "Контакты" },
+  ];
+  let buttonRefs = [];
+  for (let i = 0; i < adminItems.length; i++) {
+    buttonRefs.push(createRef());
+  }
 
+  useEffect(() => {
+    const index = adminItems.findIndex((item) => item.id === selectedItem);
+    index !== -1
+      ? setSelectedItem(adminItems[index].id)
+      : setSelectedItem("requests");
+    console.log(selectedItem);
+  }, [selectedItem]);
+
+  function handleButtonClick(ref) {
+      changeUrl(`edit=${ref.current.id}`);
+      setSelectedItem(ref.current.id)
+  }
+
+  useEffect(() => {
+    handlePageLoad();
+  }, []);
+
+  function handlePageLoad() {
+    const searchParams = new URLSearchParams(window.location.search);
+    const type = searchParams.get("edit");
+    adminItems.findIndex((item) => item.id === type) !== -1
+      ? searchParams.set("edit", type)
+      : searchParams.set("edit", "requests");
+    setSelectedItem(type);
+    changeUrl(searchParams);
+  }
+  function changeUrl(searchParams) {
+    let newurl =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname +
+      "?" +
+      searchParams.toString();
+    window.history.pushState({ path: newurl }, "", newurl);
+  }
 
   return (
-  <main className="admin">
+    <nav className="admin">
+      {adminItems.map((item, index) => (
+        <button onClick={() => {handleButtonClick(buttonRefs[index])}} ref={buttonRefs[index]} id={item.id} className="test_button">
+          {item.content}
+        </button>
+      ))}
 
-
-    <AdminMenu></AdminMenu>
-  </main>
+      {selectedItem}
+    </nav>
   );
 }
 
