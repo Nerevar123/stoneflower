616d7c04d55465c4f3b16bafad9218c1d98e7658
ie4fb <67971257+ie4fb@users.noreply.github.com>
2021-03-09 15:07:49 +0300

    done: 404 layout

--- frontend/src/components/PageNotFound.js
+++ frontend/src/components/PageNotFound.js
@@ -4,11 +4,17 @@ import { Link } from "react-router-dom";
 function PageNotFound() {
   return (
     <section className="not-found">
-      <div className="not-found__icon" />
-      <h3 className="not-found__title">404 - Страница не найдена</h3>
-      <Link className="not-found__link" to="/">
-        Вернуться на главную
-      </Link>
+      <div className="not-found__message-wrapper">
+        <h2 className="not-found__title">Ошибка 404</h2>
+        <p className="not-found__message">
+          {`Страница не найдена. 
+          Возможно, она удалена или введён
+          некорректный адрес.`}
+        </p>
+        <Link className="not-found__link" to="/">
+          Назад
+        </Link>
+      </div>
     </section>
   );
 }
