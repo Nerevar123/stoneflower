5f1d2452855fafe4d14d7cd05400ad25ad4cacc1
ie4fb <67971257+ie4fb@users.noreply.github.com>
2021-03-23 13:22:46 +0300

    added message

--- frontend/src/components/Blocker.js
+++ frontend/src/components/Blocker.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React from "react";
 function Blocker({ offer, handleOfferAcception }) {
-    return(
-        <div className="offer">
-            <div className="offer__message-container">
-                <p className="offer__message">
-                  Продолжая пользование данным сайтом, я выражаю свое согласие "Наименование Юр. лица"
-                  (Тут будет адрес) на обработку моих персональных данных с использованием интернет сервисов<br/> &laquo;Google analytics&raquo; и &laquo;Яндекс.Метрика&raquo;.<br/>
-                  С <a href={offer.path} className="offer__link">политикой</a> "Наименование Юр. лица" в отношении обработки персональных данных ознакомлен.
-                </p>
-                <button className="offer__button" onClick={handleOfferAcception}>Подтверждаю</button>
-            </div>
-
-        </div>
-        )
+  return (
+    <div className="offer">
+      <div className="offer__message-container">
+        <p className="offer__message">
+          Продолжая пользование данным сайтом, я выражаю своё согласие ООО
+          "ФИНЦентр" (Московская область, г. Ивантеевка, ул. Толмачёва, дом 1/2,
+          пом. 008), на обработку моих персональных данных с использованием
+          интернет-сервисов "Adobe Experience Cloud", "Google Analytics" и
+          "Яндекс.Метрика". <br/> С <a href={offer.path} className="offer__link">
+            Политикой
+          </a>{" "} ООО "ФИНЦентр" в отношении обработки
+          персональных данных ознакомлен.
+        </p>
+        <button className="offer__button" onClick={handleOfferAcception}>
+          Подтверждаю
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Blocker
+export default Blocker;
